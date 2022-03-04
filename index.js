9a38acaef4d0e7bcf7fc414b9822b6df40be04fe
hmarchbank <93050431+hmarchbank@users.noreply.github.com>
2022-03-04 18:45:51 +0100

    added puns to js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const npm = [
     "new phone models", "nukem's possible manifestation", 
     "nature's particle manager", "numerous pulsating mutations",
-    "Nashville plays music", "Ne pas, manager", "Node packaged masterfully"
+    "Nashville plays music", "Ne pas, manager", "Node packaged masterfully",
+    "NPM's personal messenger", 'Neolithic prancing minotaurs', 'Nicer perusal method',
+    'Nested parenthetical madness', 'Never pet mermaids', "Nutritious Pomegranate Muffins",
+    'Nascent palesticine materialist', 'Nightmare prom memories', 'Nonstick pancake maker',
+    "Nigel's personal motorcade"
+
 ]
 
 
