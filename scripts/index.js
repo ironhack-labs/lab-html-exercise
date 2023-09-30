7de0426b06c9baa4740c607aa9083829fd3772ff
Juan Rassa <jmrassa.dev@gmail.com>
2023-09-30 12:33:59 +0100

    Saved and formatted by Prettier.

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
-const changingMessageElement = document.querySelector(".header__charmMessage");
-const changingTextElement = document.querySelector(".header__charmMessageText");
+const changingMessageElement = document.querySelector('.header__charmMessage');
+const changingTextElement = document.querySelector('.header__charmMessageText');
 let messageIndex = 0;
 
 const listOfMessages = [
-  "Now Playing Mario",
-  "Neverending Pun Machine",
-  "Nascent Plasticine Materialist",
-  "Narcoleptic Pony Machine",
-  "Nattily Pimped Monster",
-  "NASA: Pluto Matters!",
-  "Nihilist Postmodern Mistake",
-  "Nodding previously Managed",
-  "Norwegian Puffin Movies",
-  "Nachos Pillage Milwaukee",
-  "Nine Pedestrians Mesmerized"
-]
+  'Now Playing Mario',
+  'Neverending Pun Machine',
+  'Nascent Plasticine Materialist',
+  'Narcoleptic Pony Machine',
+  'Nattily Pimped Monster',
+  'NASA: Pluto Matters!',
+  'Nihilist Postmodern Mistake',
+  'Nodding previously Managed',
+  'Norwegian Puffin Movies',
+  'Nachos Pillage Milwaukee',
+  'Nine Pedestrians Mesmerized',
+];
 
-changingMessageElement.addEventListener("click", () => {
+changingMessageElement.addEventListener('click', () => {
   changingTextElement.innerText = listOfMessages[messageIndex];
   if (messageIndex === listOfMessages.length - 1) {
     messageIndex = 0;
   } else {
     messageIndex++;
   }
-})
+});
